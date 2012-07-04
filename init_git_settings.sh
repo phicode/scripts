0dcfbad9fc96d2ca909ea253b53e2f03609c5577
Philipp Meinen <philipp@bind.ch>
2012-07-04 18:35:01 +0200

    add ci as an alias for commit

--- init_git_settings.sh
+++ init_git_settings.sh
@@ -2,3 +2,4 @@ git config --global core.editor vim
 git config --global user.email "philipp@bind.ch"
 git config --global user.name "Philipp Meinen"
 git config --global alias.st status
+git config --global alias.ci commit
