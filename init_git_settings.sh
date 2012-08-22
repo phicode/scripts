47c30344700540017ad101d72c9e63538050af24
Philipp Meinen <philipp@bind.ch>
2012-08-22 21:54:39 +0200

    make 'git st' use short log format

--- init_git_settings.sh
+++ init_git_settings.sh
@@ -22,5 +22,5 @@
 git config --global core.editor vim
 git config --global user.email "philipp@bind.ch"
 git config --global user.name "Philipp Meinen"
-git config --global alias.st status
+git config --global alias.st "status -s -b"
 git config --global alias.ci commit
