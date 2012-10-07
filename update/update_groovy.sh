1813e5675f8237bb93281b6088e3444ff2576949
Philipp Meinen <philipp@bind.ch>
2012-10-07 21:13:57 +0200

    update from mixed commit

--- update/update_groovy.sh
+++ update/update_groovy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2012 Philipp Meinen <philipp@bind.ch>
+# Copyright (c) 2012 Patrick Huber <stackmagic@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
